e7f874dcf693a8a77f347d811205906a8609d1d7
Ru Ji <ruji@v1040-wn-rt-a-72-128.campus-dynamic.uwaterloo.ca>
2023-08-10 19:09:46 -0400

    x

--- password.h
+++ password.h
@@ -6,7 +6,7 @@ typedef struct node{
     struct node * next;
 } node_t;
 
-seL4_Bool CheckForNullSecret(int userp, int users);
+seL4_Bool CheckForNullSecret(char * userp, char * users);
 seL4_Bool PwasFound(node_t * free_head, node_t * temp);
-seL4_Bool EndOfUsedListFound (node_t *used_head, node_t *free_head, node_t * temp, int userp, int users);
-seL4_Bool CheckForPresenceOfP (node_t *used_head, node_t *free_head, int userp, int users);
+seL4_Bool EndOfUsedListFound (node_t *used_head, node_t *free_head, node_t * temp, char * userp, char * users);
+seL4_Bool CheckForPresenceOfP (node_t *used_head, node_t *free_head, char * userp, char * users);
